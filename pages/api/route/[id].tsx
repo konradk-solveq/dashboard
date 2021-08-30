a0bddc3864a8744074d22b94d308aa3113fd1ea8
Michal Aniol <michal.aniol@solveq.io>
2021-08-30 22:17:55 +0200

    API Middlewares doesnt work

--- pages/api/route/[id].tsx
+++ pages/api/route/[id].tsx
@@ -6,10 +6,25 @@ import { getToken } from '../../../components/utils/getToken';
 const apiUrl = process.env.API_URL;
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const token = await getToken(req);
+    // const token = await getToken(req);
+    // if (!token) {
+    //     return res.status(401).send();
+    // }
 
     if (req.method === 'GET') {
-        return res.status(200).json({ token });
+        const id = req.url.split('?')[1];
+        const { data, request } = await axios.get(
+            `${apiUrl}/routes/route/`,// + id,
+            {
+                // headers: { authorization: `Bearer ${token}` },
+                params: {
+                    // id: req.query.id,
+                    id,
+                },
+            },
+        );
+
+        return res.status(200).json(data);
     }
 
     return res.status(404).send();
--- pages/routes/[id].tsx
+++ pages/routes/[id].tsx
@@ -1,28 +1,30 @@
 import { useRouter } from 'next/dist/client/router';
-import { Flex } from 'theme-ui';
+
+export const getStaticPaths = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/route`)
+    // const res = await fetch(`${process.env.API_URL}/routes/route`)
+    const data = await res.json()
+
+    const paths = data.map(e => ({
+        params: e,
+    }))
+
+    return {
+        paths,
+        fallback: true
+    };
+}
 
 export const getStaticProps = async (context) => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/route/${context.params.id}`);
     // const res = await fetch(`${process.env.API_URL}/routes/route/${context.params.id}`);
-    const res = await fetch(`${process.env.API_URL}/api/route/TzxaqyWVHyv513zOYkVQAdBY3pR_erK1`);
     const data = await res.json();
+
     return { props: { data } };
 }
 
-export function getStaticPaths(context) {
-    return { paths: [], fallback: true };
-}
+
 export default function Page(props) {
-    console.log('%c data:', 'background: #ffcc00; color: #003300', props.data)
     const router = useRouter();
-    return <Flex
-        sx={{
-            flexDirection: 'column',
-        }}>
-        <div>
-            {router.query.id}
-        </div>
-        <div>
-            --- {props.isPublic} ---
-        </div>
-    </Flex>;
+    return <>{router.query.id}</>;
 }
