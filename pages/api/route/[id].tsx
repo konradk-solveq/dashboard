0f570750addcd6928d568d4e67cbbe61d331a3d6
Michal Aniol <michal.aniol@solveq.io>
2021-09-01 21:50:25 +0200

    lousy try

--- pages/api/route/[id].tsx
+++ pages/api/route/[id].tsx
@@ -6,26 +6,24 @@ import { getToken } from '../../../components/utils/getToken';
 const apiUrl = process.env.API_URL;
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'GET') {
-    const token = await getToken(req);
-    if (!token) {
-      return res.status(401).send();
+    if (req.method === 'GET') {
+
+        const token = await getToken(req);
+
+        if (!token) {
+            return res.status(401).send();
+        }
+
+        const { id } = req.query;
+
+        const { data } = await axios.get(
+            `${apiUrl}/routes/route/${id}`,
+            {
+                headers: { authorization: `Bearer ${token}` },
+            },
+        );
+
+        return res.status(200).json(data);
     }
-    const { id } = req.query
-    const { data, request } = await axios.get(
-      `${apiUrl}/routes/route/${id}`,
-      {
-        headers: { authorization: `Bearer ${token}` },
-        params: {
-          name: req.query.name,
-          recommended: req.query.recommended,
-          public: req.query.public,
-          page: req.query.page,
-          limit: req.query.limit,
-        },
-      },
-    );
-    return res.status(200).json(data);
-  }
-  return res.status(404).send();
+    return res.status(404).send();
 };
--- pages/routes/[id].tsx
+++ pages/routes/[id].tsx
@@ -1,30 +1,28 @@
 import { useRouter } from 'next/dist/client/router';
 
 export const getStaticPaths = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/route`)
-    // const res = await fetch(`${process.env.API_URL}/routes/route`)
-    const data = await res.json()
-
-    const paths = data.map(e => ({
-        params: e,
-    }))
-
     return {
-        paths,
-        fallback: true
-    };
+        paths: [
+            {
+                params: {
+                    id: 'wfawUc2aEo4ZJRX9rj4SqyTcPzEVc2UU',
+                }
+            }
+        ],
+        fallback: false,
+    }
 }
 
 export const getStaticProps = async (context) => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/route/${context.params.id}`);
-    // const res = await fetch(`${process.env.API_URL}/routes/route/${context.params.id}`);
     const data = await res.json();
+    // const data = await JSON.stringify(res);
 
     return { props: { data } };
 }
 
-
 export default function Page(props) {
+    console.log('%c props:', 'background: #ffcc00; color: #003300', props)
     const router = useRouter();
     return <>{router.query.id}</>;
 }
