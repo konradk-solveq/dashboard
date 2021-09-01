437a4c9d4522eb26bd0e0da0d526dac02d9aa567
Rafał Pietrzak <rpietrzak@pixelage.pl>
2021-09-01 18:43:36 +0200

    add next api rute/id endpoint

--- pages/api/route/[id].tsx
+++ pages/api/route/[id].tsx
@@ -6,26 +6,26 @@ import { getToken } from '../../../components/utils/getToken';
 const apiUrl = process.env.API_URL;
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    // const token = await getToken(req);
-    // if (!token) {
-    //     return res.status(401).send();
-    // }
-
-    if (req.method === 'GET') {
-        const id = req.url.split('?')[1];
-        const { data, request } = await axios.get(
-            `${apiUrl}/routes/route/`,// + id,
-            {
-                // headers: { authorization: `Bearer ${token}` },
-                params: {
-                    // id: req.query.id,
-                    id,
-                },
-            },
-        );
-
-        return res.status(200).json(data);
+  if (req.method === 'GET') {
+    const token = await getToken(req);
+    if (!token) {
+      return res.status(401).send();
     }
-
-    return res.status(404).send();
+    const { id } = req.query
+    const { data, request } = await axios.get(
+      `${apiUrl}/routes/route/${id}`,
+      {
+        headers: { authorization: `Bearer ${token}` },
+        params: {
+          name: req.query.name,
+          recommended: req.query.recommended,
+          public: req.query.public,
+          page: req.query.page,
+          limit: req.query.limit,
+        },
+      },
+    );
+    return res.status(200).json(data);
+  }
+  return res.status(404).send();
 };
