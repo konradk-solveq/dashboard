2421e282cec6c3a919168c53f4e82872ff7384c9
Mateusz Odelga <mateusz@odel.ga>
2021-09-02 23:02:48 +0200

    MK-660 Use cookies sent by the user as authorization in route view

--- components/typings/Route.ts
+++ components/typings/Route.ts
@@ -1,4 +1,62 @@
-export interface Route {
+export class ImageData {
+    url: string;
+    width: number;
+    height: number;
+}
+
+export class Variants {
+    vertical: ImageData[];
+    share: ImageData[];
+    square: ImageData[];
+    horizontal: ImageData[];
+}
+
+export class ImagesDTO {
     id: string;
-    name: string;
+    type: 'map' | 'photo';
+    variants: Variants;
+}
+
+export class LocalizedEnum {
+    enumValue: string;
+    i18nValue: string;
+}
+
+export class LocalizedEnumOptionsArray<T> {
+    options: LocalizedEnum[];
+    values: T[];
+}
+
+export class ReactionsDTO {
+    like: number;
+    wow: number;
+    love: number;
+}
+
+export class Route {
+    public id: string;
+    public name: string;
+    author: string;
+    difficulty?: LocalizedEnumOptionsArray<any>;
+    ownerId: string;
+    surface?: LocalizedEnumOptionsArray<any>;
+    description: {
+        short: string;
+        long: string;
+    };
+    tags: LocalizedEnumOptionsArray<any>;
+    location: string;
+    recommended: boolean;
+    public path: Array<number[]>;
+    public images: ImagesDTO[];
+    distance: number;
+    distanceToRoute?: number;
+    time: number;
+    isPublic: boolean;
+    isFeatured: boolean;
+    createdAt: Date;
+    publishedAt: Date;
+    downloads: number;
+    reactions: ReactionsDTO;
+    reaction: string;
 }
--- pages/routes/[id].tsx
+++ pages/routes/[id].tsx
@@ -3,31 +3,37 @@ import { useRouter } from 'next/dist/client/router';
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 import { Box, Flex } from 'theme-ui';
+import { Route } from '../../components/typings/Route';
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-    const { id } = params;
-    const result = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/route/${id}`);
-    // const result = await fetch(`https://ckan2.multimediagdansk.pl/gpsPositions`);
-    const data = await result.json();
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { id } = context.params;
+    const result = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/route/${id}`, {
+        headers: context.req.headers as Record<string, string>,
+    });
+    const data = (await result.json()) as Route;
 
     return { props: { data } };
-}
+};
 
-const Page = (props) => {
-
+const Page: React.FC<{ data: Route }> = ({ data }) => {
     const router = useRouter();
-    if (router.isFallback) return <div>Loading...</div>;
+    if (router.isFallback) {
+        return <div>Loading...</div>;
+    }
 
-    console.log('%c data:', 'background: #ffcc00; color: #003300', props.data)
-    return <Flex sx={{
-        flexDirection: 'column',
-    }}>
-        <Box>id: {router.query.id}</Box>
-        {/* {data && <>
-            <Box>nazwa: {data.name}</Box>
-            <Box>czas: {data.time}</Box>
-        </>} */}
-    </Flex>;
-}
+    return (
+        <Flex
+            sx={{
+                flexDirection: 'column',
+            }}
+        >
+            <Box>id: {router.query.id}</Box>
+            <>
+                <Box>nazwa: {data?.name}</Box>
+                <Box>czas: {data?.time}</Box>
+            </>
+        </Flex>
+    );
+};
 
 export default Page;
