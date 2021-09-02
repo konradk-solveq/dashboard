cd009215ce47ef23f98ddc6be7924c1fc8876def
Michal Aniol <michal.aniol@solveq.io>
2021-09-02 10:48:23 +0200

    good fetch

--- pages/routes/[id].tsx
+++ pages/routes/[id].tsx
@@ -1,28 +1,17 @@
 import { useRouter } from 'next/dist/client/router';
-
-export const getStaticPaths = async () => {
-    return {
-        paths: [
-            {
-                params: {
-                    id: 'wfawUc2aEo4ZJRX9rj4SqyTcPzEVc2UU',
-                }
-            }
-        ],
-        fallback: false,
-    }
-}
-
-export const getStaticProps = async (context) => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/route/${context.params.id}`);
-    const data = await res.json();
-    // const data = await JSON.stringify(res);
-
-    return { props: { data } };
-}
+import { useState } from 'react';
+import useSWR from 'swr';
+const fetcher = (url: string) => {
+    return fetch(`${url}`).then((r) => r.json());
+};
 
 export default function Page(props) {
-    console.log('%c props:', 'background: #ffcc00; color: #003300', props)
+
     const router = useRouter();
+    const [url, setUrl] = useState(`/api/route/${router.query.id}`);
+    const { data } = useSWR(url, fetcher);
+
+
+    console.log('%c props:', 'background: #ffcc00; color: #003300', data)
     return <>{router.query.id}</>;
 }
