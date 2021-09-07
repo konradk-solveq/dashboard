814b953569deea9792007286adf525e222b78558
Michal Aniol <michal.aniol@solveq.io>
2021-09-07 22:57:40 +0200

    input forms

---
+++ components/forms/inputForm.tsx
@@ -0,0 +1,71 @@
+import { useContext, useEffect, useState } from 'react';
+import { useRouter } from 'next/dist/client/router';
+import useSWR from 'swr';
+import { Box, Flex, Button, Label, Input } from 'theme-ui';
+import EventsContext from '../contexts/EventsContext';
+import ManageContext from '../contexts/ManageContext';
+import fetcher from '../../helpers/fetcher';
+import { Route } from '../typings/Route';
+
+interface Props {
+    title: string;
+    value: any;
+    setValue: (e) => void;
+}
+
+const InputForm: React.FC<Props> = ({
+    title,
+    value,
+    setValue,
+}) => {
+
+
+    const [edit, setEdit] = useState(false)
+
+    const heandleEdit = () => {
+        setEdit(!edit)
+    }
+
+    return (
+        <Flex sx={{
+            justifyContent: 'space-between',
+            alignItems: 'flex-end',
+        }}>
+            <Box>
+                <Label sx={{ mt: '5px' }}>{title}</Label>
+                {edit && <Input
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                    sx={{
+                        bg: '#fff',
+                        p: '2px',
+                        fontFamily: 'din-b',
+                        width: '250px'
+                    }} />}
+
+                {!edit && <Box
+                    sx={{
+                        fontFamily: 'din-b',
+                        p: '3px',
+                        width: '250px'
+                    }}>
+                    {value}
+                </Box>}
+            </Box>
+            <Button
+                className='sys-btn'
+                type='button'
+                sx={{
+                    py: '3px',
+                    px: '10px',
+                    height: '28px',
+                    ml: '20px',
+                    bg: edit ? '#888' : 'primary'
+                }}
+                onClick={heandleEdit}
+            >edytuj</Button>
+        </Flex>
+    )
+}
+
+export default InputForm;
