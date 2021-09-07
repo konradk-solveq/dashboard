ac759409c5f24b4a5f045272c04e2f48e3d595b4
Rafał Pietrzak <rpietrzak@pixelage.pl>
2021-09-07 13:18:54 +0200

    fix existing endpoints

--- pages/api/cycling-map/manage/[id]/index.ts
+++ pages/api/cycling-map/manage/[id]/index.ts
@@ -1,16 +1,13 @@
-// @ts-nocheck
-import { apiHandler } from '../../../../../helpers/apiHandler';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { apiHandler, Q } from '../../../../../helpers/apiHandler';
 
 export default apiHandler({
     delete: deleteMapHandler,
 });
 
-async function deleteMapHandler(req: NextApiRequest, res: NextApiResponse) {
+async function deleteMapHandler(req: NextApiRequest & Q, res: NextApiResponse) {
     const { id } = req.query;
-    const { token, axios } = req.locals;
-    const { data } = await axios.delete(`/cycling-map/manage/${id}`, {
-        headers: { authorization: `Bearer ${token}` },
-    });
+    const { axios } = req.locals;
+    const { data } = await axios.delete(`/cycling-map/manage/${id}`);
     return res.status(200).json(data);
 }
--- pages/api/routes/route/[id].tsx
+++ pages/api/routes/route/[id].tsx
@@ -1,30 +1,13 @@
-// @ts-nocheck
 import type { NextApiRequest, NextApiResponse } from 'next';
-import axios from 'axios';
-import { getToken } from '../../../../components/utils/getToken';
-import { NextApiHandler } from 'next-auth/internals/utils';
+import { apiHandler, Q } from '../../../../helpers/apiHandler';
 
-const apiUrl = process.env.API_URL;
+export default apiHandler({
+    get: getHandler,
+});
 
-const route: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const token = await getToken(req);
-    if (!token) {
-        return res.status(401).send();
-    }
-
+async function getHandler(req: NextApiRequest & Q, res: NextApiResponse) {
     const { id } = req.query;
-    const { data } = await axios.get<{}>(
-        `${apiUrl}/routes/route/${id}`,
-        {
-            headers: { authorization: `Bearer ${token}` },
-        },
-    )
-
-    if (data) {
-        return res.status(200).json(data);
-    } else {
-        return res.status(404).end();
-    }
-};
-
-export default route;
+    const { axios } = req.locals;
+    const { data } = await axios.get(`/routes/route/${id}`);
+    return res.status(200).json(data);
+}
