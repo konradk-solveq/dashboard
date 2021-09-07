c0888d740c2bb775c3ea30b34e471a85ef81eeb7
Michal Aniol <michal.aniol@solveq.io>
2021-09-07 05:43:34 +0200

    data routes

--- pages/routes/route/[id].tsx
+++ pages/routes/route/[id].tsx
@@ -13,11 +13,17 @@ const Page = () => {
     const api = useContext(ManageContext);
     const id = router.query.id as string;
     const { data, error } = useSWR<Route, any>(`/api/routes/route/${id}?${events.getRouteUpdates(id)}`, fetcher);
+    console.log('%c data:', 'background: #ffcc00; color: #003300', data)
 
     if (router.isFallback) return <div>Loading...</div>;
     if (!data) {
         return <div>Loading...</div>;
     }
+
+    const difficulty_options = data.difficulty?.options;
+    const surface_options = data.surface?.options;
+    const tags_options = data.tags?.options;
+
     return (
         <Flex
             sx={{
@@ -33,14 +39,30 @@ const Page = () => {
             {data && (
                 <>
                     <Box>nazwa: {data.name}</Box>
+                    <Box>autor: {data.author}</Box>
+                    <Box>ownerId: {data.ownerId}</Box>
                     <Box>czas: {data.time}</Box>
+                    <Box>dystans: {data.distance}</Box>
+                    <Box>trudność: {data.time}</Box>
+                    {data.description && <>
+                        <Box>opis krótki: {data.description.short}</Box>
+                        <Box>opis długi: {data.description.long}</Box>
+                    </>}
+                    <Box>lokalizacja: {data.location}</Box>
+                    <Box>rekomandowane: {data.recommended}</Box>
+                    <Box>pobrania: {data.downloads}</Box>
+                    <Box>lajki: {data.reactions.like}</Box>
+                    {/* <Box>wow: {data.reactions.wow}</Box>
+                    <Box>love: {data.reactions.love}</Box> */}
+                    <Box>reaction: {data.reaction}</Box>
+                    <Box>polecana: {data.isFeatured}</Box>
+                    <Button>
+                        <span onClick={() => (data.isPublic ? api.unpublish(id) : api.publish(id))}>
+                            {data.isPublic ? 'Zrób niepublicznym' : 'Upublicznij'}
+                        </span>
+                    </Button>
                 </>
             )}
-            <Button>
-                <span onClick={() => (data.isPublic ? api.unpublish(id) : api.publish(id))}>
-                    {data.isPublic ? 'Zrób niepublicznym' : 'Upublicznij'}
-                </span>
-            </Button>
         </Flex>
     );
 };
