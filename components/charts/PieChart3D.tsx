e83b8d86e431319f17b71d8d21735f0cbb4d9b6a
Michal Aniol <michal.aniol@solveq.io>
2021-10-18 10:36:36 +0200

    rebase

--- components/charts/PieChart3D.tsx
+++ components/charts/PieChart3D.tsx
@@ -15,6 +15,11 @@ const PieChart3D: React.FC<{ data: any, title: string }> = ({
             options={{
                 title: title,
                 is3D: true,
+                slices: {
+                    0: { color: '#68B028' },
+                    1: { color: '#69b3a2' },
+                    3: { color: '#cf0f36' },
+                  },
             }}
             rootProps={{ 'data-testid': '2' }}
         />
