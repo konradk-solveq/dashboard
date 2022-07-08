9e9e433df2c4c1f0120adcaa515972868b0b3f18
paltoalko <martas90909@me.com>
2022-07-08 16:31:57 +0200

    fix: Translation page styling

--- components/translation/LanguageRow.tsx
+++ components/translation/LanguageRow.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container, Checkbox, Input, InputLabel } from '@mui/material/';
+import { Button, Container, Checkbox, Input, Typography } from '@mui/material/';
 
 const LanguageRow: React.FC<{
     name: string;
@@ -12,37 +12,52 @@ const LanguageRow: React.FC<{
     setter: (field, value) => void;
 }> = ({ setter, code, name, icon, isActive, saveHandler, newLanguage, deleteHandler }) => {
     return (
-        <Container sx={{ gap: 2, gridTemplateColumns: '50px 180px 2fr 40px 100px 100px', marginBottom: '10px' }}>
+        <Container
+            sx={{
+                display: 'grid',
+                gap: 2,
+                gridTemplateColumns: '0.5fr 1fr 10fr 1fr 1fr 1fr',
+                alignItems: 'center',
+            }}
+        >
             <Input
-                sx={{ backgroundColor: `${!newLanguage ? 'lightgrey' : 'white'}` }}
+                className="input-black"
+                disableUnderline={true}
+                sx={{ backgroundColor: `${!newLanguage ? 'lightgrey' : 'white'}`, fontSize: '14px' }}
                 placeholder="kod"
                 disabled={!newLanguage ? true : false}
                 value={code}
                 onChange={(e) => setter('code', e.target.value)}
             ></Input>
             <Input
-                sx={{ backgroundColor: 'white' }}
+                className="input-black"
+                disableUnderline={true}
+                sx={{ backgroundColor: 'white', fontSize: '14px' }}
                 placeholder="nazwa"
                 value={name || ''}
                 onChange={(e) => setter('name', e.target.value)}
             ></Input>
             <Input
-                sx={{ backgroundColor: 'white' }}
+                className="input-black"
+                disableUnderline={true}
+                sx={{ backgroundColor: 'white', fontSize: '14px' }}
                 placeholder="ikona"
                 value={icon || ''}
                 onChange={(e) => setter('icon', e.target.value)}
             ></Input>
-            <InputLabel sx={{ p: '8px' }}>
+            <Typography sx={{ p: '8px' }}>
                 <Checkbox
-                    sx={{ backgroundColor: 'white' }}
+                    sx={{ backgroundColor: 'white', fontSize: '14px' }}
                     checked={isActive}
                     onChange={(e) => setter('isActive', e.target.checked)}
                 />
-            </InputLabel>
-            <Button sx={{ bg: 'green' }} onClick={saveHandler}>
+            </Typography>
+            <Button variant="contained" color="success" onClick={saveHandler}>
                 Zapisz
             </Button>
-            <Button onClick={deleteHandler}>Usuń</Button>
+            <Button variant="contained" color="error" onClick={deleteHandler}>
+                Usuń
+            </Button>
         </Container>
     );
 };
--- components/translation/TranslationAddForm.tsx
+++ components/translation/TranslationAddForm.tsx
@@ -1,30 +1,43 @@
 import React from 'react';
-import { Box, Button, Container, Input, InputLabel } from '@mui/material/';
+import { Box, Button, Container, Input, Typography } from '@mui/material/';
 
 const TranslationAddForm = ({ newUiTranslations, setNewUiTranslationsValue, addUiTranslationAndNotify }) => {
     const { code, version, file } = newUiTranslations;
     const disabledAddTranslation = code && version && file ? false : true;
     return (
         <>
-            <Container sx={{ display: 'grid', gridTemplateColumns: '50px 180px 1fr', mb: '10px' }}>
-                <InputLabel>Kod</InputLabel>
-                <InputLabel>Wersja</InputLabel>
-                <InputLabel>Plik z tłumaczeniem UI</InputLabel>
+            <Container sx={{ display: 'grid', gridTemplateColumns: '50px 180px 1fr', mb: '10px', gap: 2 }}>
+                <Typography variant="h5">Kod</Typography>
+                <Typography variant="h5">Wersja</Typography>
+                <Typography variant="h5">Plik z tłumaczeniem UI</Typography>
             </Container>
-            <Container sx={{ display: 'grid', gridTemplateColumns: '50px 180px 1fr', mb: '10px' }}>
-                <Input value={code} onChange={(e) => setNewUiTranslationsValue('code')(e.target.value)}></Input>
-                <Input value={version} onChange={(e) => setNewUiTranslationsValue('version')(e.target.value)}></Input>
+            <Container sx={{ display: 'grid', gridTemplateColumns: '50px 180px 1fr', mb: 2, gap: 2 }}>
                 <Input
+                    className="document-input-form document-select-form"
+                    disableUnderline={true}
+                    value={code}
+                    onChange={(e) => setNewUiTranslationsValue('code')(e.target.value)}
+                ></Input>
+                <Input
+                    className="document-input-form document-select-form"
+                    disableUnderline={true}
+                    value={version}
+                    onChange={(e) => setNewUiTranslationsValue('version')(e.target.value)}
+                ></Input>
+                <Input
+                    className="document-input-form document-select-form"
+                    disableUnderline={true}
                     type="file"
                     accept="application/json"
                     multiple={false}
                     onChange={(e) => setNewUiTranslationsValue('file')(e.target.files[0])}
                 ></Input>
             </Container>
-            <Container sx={{ display: 'grid', gridTemplateColumns: '4fr 200px 4fr', mb: '40px' }}>
-                <Box></Box>
+            <Container sx={{ display: 'flex', justifyContent: 'center' }}>
                 <Button
                     sx={{ textAlign: 'center' }}
+                    variant="contained"
+                    color="success"
                     onClick={() => addUiTranslationAndNotify(newUiTranslations)}
                     {...(disabledAddTranslation ? { bg: 'lightgrey', disabled: true } : {})}
                 >
