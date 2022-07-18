a8e09a4cd4757260c13a7c7e6de842f18be1c373
paltoalko <martas90909@me.com>
2022-07-18 09:49:05 +0200

    fix: added style for alerts for  notification box

--- components/translation/NotificationBox.tsx
+++ components/translation/NotificationBox.tsx
@@ -3,6 +3,7 @@ import { Box, Alert, Snackbar, Typography } from '@mui/material/';
 
 const NotificationBox = ({ children }) => {
     const [open, setOpen] = useState(true);
+    let alertSeverity;
 
     const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
         if (reason === 'clickaway') {
@@ -12,11 +13,21 @@ const NotificationBox = ({ children }) => {
         setOpen(false);
     };
 
+    if (children == ('Wystąpił błąd' || 'Wystąpił błąd ładowania danych')) {
+        alertSeverity = 'error';
+    } else if (children == 'Zapisano') {
+        alertSeverity = 'success';
+    } else if (children == 'Usunięto') {
+        alertSeverity = 'warning';
+    } else {
+        alertSeverity = 'info';
+    }
+
     return (
         <Box>
             <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
                 <Alert
-                    severity="info"
+                    severity={alertSeverity}
                     sx={{ width: '100%', display: 'flex', alignItems: 'center' }}
                     onClose={handleClose}
                 >
